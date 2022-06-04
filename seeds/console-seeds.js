a38247c4e9b714636ee877f61c5eb9a83ac434e5
blur <damienhebert710@gmail.com>
2022-06-04 13:38:02 -0500

    seeds files made and edited

---
+++ seeds/console-seeds.js
@@ -0,0 +1,24 @@
+const { Console } = require('../models');
+
+const consoleData = [
+    {
+        console_name: 'Xbox One', 
+    }, 
+    {
+        console_name: 'PS4'
+    }, 
+    {
+        console_name: 'Nintendo Switch'
+    }, 
+    {
+        console_name: 'Xbox 360'
+    }, 
+    {
+        console_name: 'PS3'
+    }, 
+    {
+        console_name: 'Nintendo Wii'
+    }
+    
+];
+ const consoleCategories = () => Con
