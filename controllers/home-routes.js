b2c5c1a11d0ae290348c1bd25ff0d5c22c13b7b6
Ian <Ian.Mohn1@gmail.com>
2022-06-07 04:35:14 -0500

    created a get route for rendering a signup form, commented out for now

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,10 +5,27 @@ const sequelize = require('../config/connection');
 router.get('/', (req, res) => {
     res.render('homepage');
 });
+// --------------
+// I need to make some changes to lines 10-15 before we test this route
+//router.get('/signup', (req, res) => {
+ //   if(req.session.loggedIn) {
+ //       res.redirect('/');
+ //       return;
+ //   }
+ // res.render("signup")
+// });
+//-----------------
+
 // Just the initial setup for when we create the code for the login/user-data
 router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
     res.render('login');
 });
+
+module.exports = router;
 
 
 
