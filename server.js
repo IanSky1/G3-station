556c466e89f0b97f83157b5f950d45c9635ea106
Ian <Ian.Mohn1@gmail.com>
2022-06-07 14:00:10 -0500

    updated changes to server.js file with declared imports

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
-//Loads the express module
 const express = require('express');
-//Creates our express server
-const app = express();
-const port = 3000;
-//Serves static files (we need it to import a css file)
-app.use(express.static('public'))
-//Sets a basic route
-app.get('/', (req, res) => res.send('Hello World !'));
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
-//Makes the app listen to port 3000
-app.listen(port, () => console.log(`App listening to port ${port}`));
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+const sequelize = require('./config/connection');
+const sequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
+
+const hbs = exphbs.create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+const sess = {
+    secret: process.env.g3_station_db,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new sequelizeStore({
+        db: sequelize,
+    }),
+};
+
+app.use(session(sess));
+
+app.use((req, res, next) => {
+    next();
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+
+});
