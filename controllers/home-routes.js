678775cfa9461a903610a575a3758597873dc6b4
Ian <Ian.Mohn1@gmail.com>
2022-06-07 20:10:55 -0500

    needed a lower case 's' instead of a capital 'S'. I really dislike sequelize now

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-// Const { Console, Game, Genre } = require("../models"); May want to change the names of these?
+const { Console, Game, Genre} = require("../models");
 
 router.get('/', (req, res) => {
     res.render('homepage');
