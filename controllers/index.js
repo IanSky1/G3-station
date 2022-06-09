54626dd8f0165cbbd64b2d83ba79ce496ec8a122
Ian <Ian.Mohn1@gmail.com>
2022-06-09 15:46:26 -0500

    created separate branch to resolve merge conflicts and imported new code from previous deleted feature branch for post-routes.js and comment-routes.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
-// const apiRoutes = require('./api');
+const dashboardRoutes = require('./dashboard-routes.js')
+const apiRoutes = require('./api');
 
-// These routes haven't been set up yet, just declaring them after creating the files. Still need to decide on the api we are using. 
+
 
 router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
+
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res)=> {
     res.status(404).end();
